454c41b57698c8d0bff0b1a96633d5a218597035
irfanpule <irfan.pule2@gmail.com>
2020-06-25 10:28:57 +0700

    Initial Commit

---
+++ Wispung/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wispung
+//
+//  Created by Muhammad Irfan on 25/06/20.
+//  Copyright © 2020 Muhammad Irfan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
